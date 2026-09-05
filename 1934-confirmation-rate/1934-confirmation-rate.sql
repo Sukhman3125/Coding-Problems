663b20cee16d6a3196c87e56fdd002e1a32c64b9
Sukhman3125 <141159003+Sukhman3125@users.noreply.github.com>
2026-09-05 13:23:23 +0530

    Time: 706 ms (65.27%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -2,7 +2,7 @@ SELECT s.user_id AS user_id,
     ROUND(
         IF(COUNT(c.time_stamp) = 0, 
             0,
-            SUM(IF(c.action = "confirmed", 1, 0)) / COUNT(c.time_stamp)
+            COUNT(IF(c.action = "confirmed", 1, NULL)) / COUNT(c.time_stamp)
         )
     ,2) AS confirmation_rate
 FROM Signups s
