c39e0151bc286a0e26df1b112c3d69ebcd8410be
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-15 20:02:22 +0530

    Time: 3 ms (63.7%), Space: 19 MB (23.88%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,30 @@
+class Solution {
+using t = tuple<int,int,int>; 
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& f : flights) {
+            adj[f[0]].push_back({f[1], f[2]});
+        }
+        priority_queue<t, vector<t>, greater<t>> pq;
+        pq.push({0, src, 0});
+        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
+        dist[src][0] = 0;
+        while (!pq.empty()) {
+            auto [cost, u, stops] = pq.top();
+            pq.pop();
+            if (u == dst)
+                return cost;
+            if (stops == k + 1)
+                continue;
+            for (auto [v, price] : adj[u]) {
+                int newCost = cost + price;
+                if (newCost < dist[v][stops + 1]) {
+                    dist[v][stops + 1] = newCost;
+                    pq.push({newCost, v, stops + 1});
+                }
+            }
+        }
+        return -1;
+    }
+};
