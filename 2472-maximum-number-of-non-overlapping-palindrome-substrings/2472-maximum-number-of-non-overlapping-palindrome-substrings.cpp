ff0e2bc925a90cbd2aa7aac3cea01470dc110ada
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-15 13:32:54 +0530

    Time: 0 ms (100%), Space: 9.4 MB (85.86%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -9,63 +9,81 @@ private:
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
+
         if (k == 1)
             return n;
+
         vector<pair<int, int>> palins;
 
         for (int i = 0; i < n; i++) {
-            // odd len
+            // Odd length palindrome
             int l = i - 1, r = i + 1;
+
             while (l >= 0 && r < n) {
                 if (s[l] != s[r])
                     break;
+
                 if (r - l + 1 >= k) {
                     palins.push_back({l, r});
                     break;
                 }
-                l--, r++;
+
+                l--;
+                r++;
             }
 
-            // even len left
-            l = i - 1, r = i;
+            // Even length palindrome: center between i-1 and i
+            l = i - 1;
+            r = i;
+
             while (l >= 0 && r < n) {
                 if (s[l] != s[r])
                     break;
+
                 if (r - l + 1 >= k) {
                     palins.push_back({l, r});
                     break;
                 }
-                l--, r++;
+
+                l--;
+                r++;
             }
 
-            // even len right
-            l = i, r = i + 1;
+            // Even length palindrome: center between i and i+1
+            l = i;
+            r = i + 1;
+
             while (l >= 0 && r < n) {
                 if (s[l] != s[r])
                     break;
+
                 if (r - l + 1 >= k) {
                     palins.push_back({l, r});
                     break;
                 }
-                l--, r++;
+
+                l--;
+                r++;
             }
         }
-        // for(auto [x,y]:palins){
-        //     cout<<x<<" - "<<y<<endl;
-        // }
-        vector<pair<int, int>> intervals(palins.begin(), palins.end());
 
-        sort(intervals.begin(), intervals.end(),
-             [](const auto& a, const auto& b) { return a.second < b.second; });
+        // Classic interval scheduling:
+        // choose intervals with the earliest ending position.
+        sort(palins.begin(), palins.end(),
+             [](const auto& a, const auto& b) {
+                 return a.second < b.second;
+             });
+
         int ans = 0;
         int lastEnd = -1;
 
-        for (auto& [l, r] : intervals) {
+        for (auto& [l, r] : palins) {
             if (l > lastEnd) {
                 ans++;
                 lastEnd = r;
             }
         }
+
         return ans;
     }
 };
