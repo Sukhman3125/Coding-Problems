e16e6e9486f598e363c1cf6f66108908cc0f2cbb
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-16 23:24:24 +0530

    Time: 650 ms (5.25%), Space: 0B (100%) - LeetHub

---
+++ 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ip,
+    COUNT(*) AS invalid_count
+FROM logs
+WHERE ip NOT REGEXP '^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$' OR 
+    ip REGEXP '(^|\\.)0[0-9]' OR 
+    CAST(SUBSTRING_INDEX(ip, '.', 1) AS UNSIGNED) > 255 OR 
+    CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) AS UNSIGNED) > 255 OR 
+    CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) AS UNSIGNED) > 255 OR 
+    CAST(SUBSTRING_INDEX(ip, '.', -1) AS UNSIGNED) > 255
+GROUP BY ip
+ORDER BY invalid_count DESC, ip DESC;
