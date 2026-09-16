764cccebaf77ec9c04d0e39ede0c6be6489d4201
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-16 16:04:06 +0530

    Time: 14 ms (52.73%), Space: 114.2 MB (79.27%) - LeetHub

---
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.back() > nums[i] && st.size()+n-i > k){
+                st.pop_back();
+            }
+            if(st.size()<k){
+                st.push_back(nums[i]);
+            }
+        }
+        return st;
+    }
+};
