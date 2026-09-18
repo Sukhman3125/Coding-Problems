aaa4ca93abcd067ccb8c9aa00cb9ee5d412f5c79
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-18 13:48:14 +0530

    Time: 62 ms (29.59%), Space: 23.3 MB (71.64%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -1,20 +1,27 @@
 class Solution {
-private:
-    void cleanUp(vector<pair<int,int>>& v){
-        int i=0, j= v.size()-1;
-        while(i<j){
-            if(v[i].first != -1) {
-                i++;
-                continue;
-            }
-            while(i<j && v[j].first == -1) j--;
-            if(i<j) swap(v[i], v[j]);
-            i++;
-        }
-        while(v.back().first == -1) v.pop_back();
-    }
 public:
     vector<string> maxNumOfSubstrings(string s) {
+        auto subs = findOccurences(s);
+        expandWindows(s, subs);
+        cleanUp(subs);
+
+        sort(subs.begin(), subs.end(),[](const auto& a, const auto& b){
+            auto& [i1,j1] = a;
+            auto& [i2,j2] = b;
+            return j1-i1+1 < j2-i2+1;
+        });
+
+        auto ans = findNonOverlappingIntervals(s, subs);
+        
+        vector<string> res;
+        for(auto [i,j]: ans){
+            res.push_back(s.substr(i, j-i+1));
+        }
+        return res;
+    }
+
+private:
+    vector<pair<int,int>> findOccurences(string& s){
         vector<pair<int,int>> subs(26,{-1,-1});
         for(int i=0;i<s.size();i++){
             char ch = s[i];
@@ -23,32 +30,32 @@ public:
             }
             subs[ch-'a'].second = i;
         }
+        return subs;
+    }
 
-        for(int i=0;i<26;i++){
-            auto& [_i,_j] = subs[i];
-            if(_i==-1) continue;
+    void expandWindows(string& s, vector<pair<int,int>>& subs){
+        for(int idx=0;idx<26;idx++){
+            auto& [i,j] = subs[idx];
+            if(i==-1) continue;
             while(1){
                 bool changed = false;
-                for(int k=_i;k<=_j;k++){
-                    auto [newS, newE] = subs[s[k]-'a'];
-                    if(newS < _i) {
+                for(int idx=i;idx<=j;idx++){
+                    auto [newI, newJ] = subs[s[idx]-'a'];
+                    if(newI < i) {
                         changed = true;
-                        _i = newS;
+                        i = newI;
                     }
-                    if(newE > _j){
+                    if(newJ > j){
                         changed = true;
-                        _j = newE;
+                        j = newJ;
                     }
                 }
                 if(!changed) break;
             }
         }
-        cleanUp(subs);
-        sort(subs.begin(), subs.end(),[](const auto& a, const auto& b){
-            auto& [i1,j1] = a;
-            auto& [i2,j2] = b;
-            return j1-i1+1 < j2-i2+1;
-        });
+    }
+
+    vector<pair<int,int>> findNonOverlappingIntervals(string& s, vector<pair<int,int>>& subs){
         vector<pair<int,int>> ans;
         for(auto [i,j]:subs){
             bool overlapped = false;
@@ -63,10 +70,20 @@ public:
             }
             if(!overlapped) ans.push_back({i,j});
         }
-        vector<string> res;
-        for(auto [i,j]: ans){
-            res.push_back(s.substr(i, j-i+1));
+        return ans;
+    }
+
+    void cleanUp(vector<pair<int,int>>& v){
+        int i=0, j= v.size()-1;
+        while(i<j){
+            if(v[i].first != -1) {
+                i++;
+                continue;
+            }
+            while(i<j && v[j].first == -1) j--;
+            if(i<j) swap(v[i], v[j]);
+            i++;
         }
-        return res;
+        while(v.back().first == -1) v.pop_back();
     }
 };
