21a6db994d58b1a8e46094ed57d661b2c71718d8
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-20 15:50:49 +0530

    Time: 75 ms (52.44%), Space: 148.5 MB (92.38%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -3,40 +3,67 @@ private:
     priority_queue<int> smallHalf;
     priority_queue<int, vector<int>, greater<int>> largeHalf;
 
-    void handleFirstTwoInsertions(int& num) {
-        if (smallHalf.empty()) { // first insertion in small
+public:
+    void addNum(int num) {
+        int m = smallHalf.size(), n = largeHalf.size();
+        
+        if (m + n == 0) {
             smallHalf.push(num);
             return;
+        } 
+        else if (m == n) {
+            handleEqualSized(num);
+        } 
+        else if (m < n) {
+            handleLargeMore(num);
+        } 
+        else {
+            handleSmallMore(num);
         }
-        // 2nd insertion
-        int prev = smallHalf.top();
-        smallHalf.pop();
-        smallHalf.push(min(num, prev));
-        largeHalf.push(max(num, prev));
+
     }
 
+    double findMedian() {
+        int m = smallHalf.size(), n = largeHalf.size();
+        if (m + n == 0) // no elements, so median 0
+            return 0;
+        
+        // odd number of elements
+        if (m > n)
+            return smallHalf.top();
+        if (n > m)
+            return largeHalf.top();
+        
+        // even number of elements
+        return (smallHalf.top() + largeHalf.top()) / 2.0;
+    }
+
+private:
     void handleEqualSized(int& num) {
         int lowerBound = smallHalf.top();
         int upperBound = largeHalf.top();
+        
         if (num <= lowerBound)
             smallHalf.push(num);
         else if (num >= upperBound)
             largeHalf.push(num);
         else
-            smallHalf.push(num); // either is fine in this case
+            smallHalf.push(num);    // either is fine in this case
+
     }
 
     void handleSmallMore(int& num) {
         int lowerBound = smallHalf.top();
-        if (num >= lowerBound) {
+        if (num >= lowerBound) {    // the lesser size pq can accept the number
             largeHalf.push(num);
             return;
         }
-        smallHalf.pop();
+        smallHalf.pop();            // since lesser size pq can't accept, we replace one element from larger
         smallHalf.push(num);
         largeHalf.push(lowerBound);
     }
-    void handleLargeMore(int& num) {
+
+    void handleLargeMore(int& num) {    // symmetrical to function above
         int upperBound = largeHalf.top();
         if (num <= upperBound) {
             smallHalf.push(num);
@@ -46,36 +73,4 @@ private:
         largeHalf.push(num);
         smallHalf.push(upperBound);
     }
-
-public:
-    void addNum(int num) {
-        int m = smallHalf.size(), n = largeHalf.size();
-        if (m + n < 2) {
-            handleFirstTwoInsertions(num);
-        } else if (m == n) {
-            handleEqualSized(num);
-        } else if (m < n) {
-            handleLargeMore(num);
-        } else {
-            handleSmallMore(num);
-        }
-    }
-
-    double findMedian() {
-        int m = smallHalf.size(), n = largeHalf.size();
-        if (m + n == 0)
-            return 0;
-        if (m > n)
-            return smallHalf.top();
-        if (n > m)
-            return largeHalf.top();
-        return (smallHalf.top() + largeHalf.top()) / 2.0;
-    }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
