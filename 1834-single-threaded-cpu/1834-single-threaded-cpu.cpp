c83749cd83cd6421f12794b7dde537daf284c731
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-23 18:16:42 +0530

    Time: 600 ms (10.41%), Space: 366.5 MB (12.41%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,36 +1,36 @@
 class Solution {
+using v = vector<int>;
 private:
-    struct cmp {
-        bool operator()(const auto& a, const auto& b) const {
-            if(a[1] != b[1]) return a[1] > b[1];
-            return a[2] > b[2];
+    struct comp{
+        bool operator()(const v& a, const v& b) const {
+            if(a[1]!=b[1]) return a[1]>b[1];
+            return a[2]>b[2];
         }
     };
-
 public:
-    vector<int> getOrder(vector<vector<int>>& tasks) {
-        int n = tasks.size();
-        for(int i=0;i<n;i++){
+    v getOrder(vector<v>& tasks) {
+        int n=tasks.size();
+        for(int i=0; i<n; i++){
             tasks[i].push_back(i);
         }
-        sort(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) { 
-            return a[0] < b[0]; 
+        sort(tasks.begin(), tasks.end(), [](const v& a, const v& b){
+            return a[0]<b[0];
         });
-        priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
-        long long currTime = 0;
-        int i=0;
-        vector<int> ans;
+        priority_queue<v, vector<v>, comp> pq;
+        long long time = 0;
+        int i = 0;
+        v ans;
         while(i<n || !pq.empty()){
-            while(i<n && tasks[i][0] <= currTime){
+            while(i<n && tasks[i][0] <= time){
                 pq.push(tasks[i]);
                 i++;
             }
             if(pq.empty()){
-                currTime = tasks[i][0];
+                time = tasks[i][0];
                 continue;
             }
-            currTime += pq.top()[1];
             ans.push_back(pq.top()[2]);
+            time += pq.top()[1];
             pq.pop();
         }
         return ans;
