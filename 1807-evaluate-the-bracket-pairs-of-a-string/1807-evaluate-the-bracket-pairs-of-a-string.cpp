077a44651531002ba9232634277926352dddcb9e
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-26 10:57:23 +0530

    Time: 230 ms (7.48%), Space: 402.9 MB (6.23%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,21 +1,55 @@
+class Trie {
+private:
+    string val = "";
+    Trie* next[26] = {nullptr};
+    void put(char ch) { 
+        this->next[ch - 'a'] = new Trie(); 
+    }
+    Trie* get(char ch) { 
+        return this->next[ch - 'a']; 
+    }
+public:
+    void insert(string key, string val){
+        Trie* temp = this;
+        for(char ch:key){
+            if(!temp->get(ch)) temp->put(ch);
+            temp = temp->get(ch);
+        }
+        temp->val = val;
+    }
+
+    string value(string word){
+        Trie* temp = this;
+        for(char ch: word){
+            if(!temp->get(ch)) return "";
+            temp = temp->get(ch);
+        }
+        return temp->val;
+    }
+};
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
-        unordered_map<string, string> mp;
-        for(auto& it:knowledge) mp[it[0]] = it[1];
+        Trie* dict = new Trie();
+        for (auto& it : knowledge)
+            dict->insert(it[0], it[1]);
         string key = "";
         bool unresolved = false;
         string ans = "";
-        for(auto it:s){
-            if(it=='(') unresolved = true;
-            else if(it==')'){
+        for (auto it : s) {
+            if (it == '(')
+                unresolved = true;
+            else if (it == ')') {
                 unresolved = false;
-                if(mp.contains(key)) ans += mp[key];
-                else ans += '?';
+                string val = dict->value(key);
+                if (val != "")
+                    ans += val;
+                else
+                    ans += '?';
                 key = "";
-            }else if(unresolved){
+            } else if (unresolved) {
                 key += it;
-            }else{
+            } else {
                 ans += it;
             }
         }
