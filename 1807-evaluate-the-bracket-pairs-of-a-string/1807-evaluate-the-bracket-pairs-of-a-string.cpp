788592a993a9de33d5866811e8104985687c42a2
Sukhmanpreet Singh <sukhman3125@gmail.com>
2026-09-26 11:04:28 +0530

    Time: 219 ms (8.1%), Space: 402.8 MB (6.23%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,6 +1,6 @@
 class Trie {
 private:
-    string val = "";
+    int idx = -1;
     Trie* next[26] = {nullptr};
     void put(char ch) { 
         this->next[ch - 'a'] = new Trie(); 
@@ -9,30 +9,31 @@ private:
         return this->next[ch - 'a']; 
     }
 public:
-    void insert(string key, string val){
+    void insert(string key, int idx){
         Trie* temp = this;
         for(char ch:key){
             if(!temp->get(ch)) temp->put(ch);
             temp = temp->get(ch);
         }
-        temp->val = val;
+        temp->idx = idx;
     }
 
-    string value(string word){
+    int valIdx(string word){
         Trie* temp = this;
         for(char ch: word){
-            if(!temp->get(ch)) return "";
+            if(!temp->get(ch)) return -1;
             temp = temp->get(ch);
         }
-        return temp->val;
+        return temp->idx;
     }
 };
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         Trie* dict = new Trie();
-        for (auto& it : knowledge)
-            dict->insert(it[0], it[1]);
+        for(int i=0;i<knowledge.size();i++){
+            dict->insert(knowledge[i][0], i);
+        }
         string key = "";
         bool unresolved = false;
         string ans = "";
@@ -41,9 +42,9 @@ public:
                 unresolved = true;
             else if (it == ')') {
                 unresolved = false;
-                string val = dict->value(key);
-                if (val != "")
-                    ans += val;
+                int idx = dict->valIdx(key);
+                if (idx != -1)
+                    ans += knowledge[idx][1];
                 else
                     ans += '?';
                 key = "";
